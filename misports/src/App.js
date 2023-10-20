4c538d8fcdf49d068d6a20c20c6e766001cb895f
MITMalini <malinij@mitesp.com>
2023-10-20 17:20:57 +0530

    10/20/2023
    DASHBOAD TRYING

--- misports/src/App.js
+++ misports/src/App.js
@@ -1,18 +1,31 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./core/login";
+import Login from "./core/pages/Login";
+import Dashboard from "./core/pages/Dashboard";
+// import SideNav from "./core/components/SideNav";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" exact element={<Login />} />
+          {/* Display SideNav only for the Home route */}
+          <Route
+            path="/home/*"
+            element={
+              <>
+                <Routes>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ misports/src/core/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/dashboard-styles.css";
+import SideNav from "../components/SideNav";
+
+const Dashboard = (props) => {
+  return (
+    <div>
+      <SideNav />
+      <div className="containerDashboard1">hi</div>
+    </div>
+  );
+};
+
+export default Dashboard;
